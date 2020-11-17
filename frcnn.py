70e6d17158e601a8fc950286cbc9038c110d20dd
Zzzyf98 <70901107+Zzzyf98@users.noreply.github.com>
2020-11-18 02:37:49 +0800

    Update frcnn.py

--- frcnn.py
+++ frcnn.py
@@ -22,7 +22,7 @@ class FRCNN(object):
         "model_path"    : 'model_data/voc_weights_resnet.pth',
         "classes_path"  : 'model_data/voc_classes.txt',
         "confidence"    : 0.5,
-        "iou"           : 0.3,
+        "iou"           : 0.5,
         "backbone"      : "resnet50"
     }
 
